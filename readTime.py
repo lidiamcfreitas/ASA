7bd2f0ace82fa81b5478124eb947550c8851e36f
lidiamcfreitas <lidiamcfreitas@gmail.com>
2015-03-21 17:30:38 +0000

    added a function to translate command time output to comma separated values

---
+++ readTime.py
@@ -0,0 +1,16 @@
+import sys
+
+def readvalues(filename):
+    """reads input configuration from filename"""
+    file = open(filename,"r")
+    res = []
+    i = 1
+    for line in file:
+        
+        if i%4 == 0:
+            x = line[:-1].split("m")
+            i = 1
+            sys.stdout.write(x[1][:-1] + ",")
+        else:
+            i += 1
+    
